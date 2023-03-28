b3c5231c2d0493d1a4e22672b0cb70327b2bbb52
Fernanda Viana <128320607+Fernanda-Marcelino@users.noreply.github.com>
2023-03-28 20:08:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function sf(){
+	let sfimg = document.querrySelector("img#imagem"); 
+	let num = Math.trunc( Math.random()*10);
+const botao = document.querySelector('#botao');
+botao.addEventListener('click', function sf() {
+	if (num == 0) {
+		sfimg.setAttribute ("src", "img/sally.jpg");
+	}
+	else if (num == 1) {
+		sfimg.setAttribute ("src", "img/img1.jpg");
+	}
+	else if (num == 2) {
+		sfimg.setAttribute ("src", "img/img2.jpg");
+	}
+	else if (num == 3) {
+		sfimg.setAttribute ("src", "img/img4.jpg");
+	}
+	else if (num == 4) {
+		sfimg.setAttribute ("src", "img/img5.jpg");
+	}
+	else if (num == 5) {
+		sfimg.setAttribute ("src", "img/img6.jpg");
+	}
+	else if (num == 6) {
+		sfimg.setAttribute ("src", "img/img7.jpg");
+	}
+	else if (num == 7) {
+		sfimg.setAttribute ("src", "img/img8.jpg");
+	}
+	else if (num == 8) {
+		sfimg.setAttribute ("src", "img/img9.jpg");
+	}
+	else if (num == 9) {
+		sfimg.setAttribute ("src", "img/img10.jpg");
+	}
+	else if (num == 10) {
+		sfimg.setAttribute ("src", "img/img11.jpg");
+	}
+} 
+) 
